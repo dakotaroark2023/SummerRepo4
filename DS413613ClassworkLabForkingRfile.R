fe5902e7b53bd6723cd015b41b78a642a4feb4c6
dakotaroark2023 <126114589+dakotaroark2023@users.noreply.github.com>
2023-04-05 12:55:16 -0400

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -2,15 +2,15 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 mpg
-# My name is Pete Magee
-# This is my coding that generates Boxplots for transmission types
+# My name is Dakota Roark
 # The data source is the mpg data table
-ggplot(data = mpg) +
+# This is my coding that generates Boxplots for transmission types
+bp<- ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
   ggtitle("Boxplots (mpg data : transmission types vs displacement)") +
   xlab("transmission types") +
-  ylab("displacement") +
-coord_flip()
+  ylab("displacement") 
+bp+ coord_flip()
   
 
 
